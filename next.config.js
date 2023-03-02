d4db2430cac7d9eb7ca0bd6045a96f3c7412caf5
masahiro96848 <feetwcpic9436@gmail.com>
2023-03-02 23:46:42 +0900

    next.config.js .prettierrcファイルを作成。.env.developmentファイルを作成

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+    experimental: {
+        appDir: true,
+    },
+    env: {
+        BASE_API_URL: process.env.BASE_API_URL,
+    },
+    reactStrictMode: true,
+    swcMinify: true,
 }
 
 module.exports = nextConfig

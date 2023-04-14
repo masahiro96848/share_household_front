2982c326d2babff72a090216192230dfb7feb793
masahiro96848 <feetwcpic9436@gmail.com>
2023-04-15 00:13:34 +0900

    UIフレームワーク mantineをインストールとどのページでも使えるように実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { MantineProvider } from '@mantine/core'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/contexts/AuthContext'
@@ -15,9 +16,15 @@ const queryClient = new QueryClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: 'light' }}
+      >
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </MantineProvider>
     </QueryClientProvider>
   )
 }
